b0bfad41eeda7d39f0817e110739d8fbacc01ca6
Nacho Ferro <you@example.com>
2022-08-19 01:46:35 -0300

    edit encriptador js and style

---
+++ scriptEncriptador.js
@@ -0,0 +1,31 @@
+const inputTexto = document.querySelector(".encryptInput")
+const message = document.querySelector(".message")
+
+function encryptButton(){
+
+    document.getElementById("dissapearWhenClicked").style.display="none";
+
+    const textoEncriptado = encriptar(inputTexto.value);
+    message.value = textoEncriptado; 
+    message.style.backgroundImage="none";
+}
+
+function encriptar(string){
+    let matrizCódigo = [["e","enter"], 
+                        ["i", "imes"], 
+                        ["a", "ai"], 
+                        ["o", "ober"]
+                        ["u", "ufat"]];
+    string = string.toLowerCase();
+
+    for( let index = 0; index < matrizCódigo.length; index++){
+
+        if(string.includes(matrizCódigo[index][0])){ //Verifica si la letra actual es una de matrizCódigo
+            string = string.replaceAll( matrizCódigo[index][0],
+                                        matrizCódigo[index][1]);
+        }
+
+    }
+
+    return string;
+}
